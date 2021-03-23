012c4c27f1f1546c83fd2d4a03605acddb3f9064
Guillem Fernandez <guillem.fernandez@enti.cat>
2021-03-23 20:36:39 +0100

    First commit

--- BattleStar/GameScene.swift
+++ BattleStar/GameScene.swift
@@ -10,76 +10,68 @@ import GameplayKit
 
 class GameScene: SKScene {
     
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
+    private var pilotNode : SKSpriteNode!
+    private let flightAnimation = [SKTexture(imageNamed: "Fly (1)"),
+                                   SKTexture(imageNamed: "Fly (2)")]
+    private let shootAnimation = [SKTexture(imageNamed: "Shoot (1)"),
+                                  SKTexture(imageNamed: "Shoot (2)"),
+                                  SKTexture(imageNamed: "Shoot (3)"),
+                                  SKTexture(imageNamed: "Shoot (4)"),
+                                  SKTexture(imageNamed: "Shoot (5)"),]
+    private var flyAction: SKAction!
+    private var shootAction: SKAction!
+    
+    private let flyActionKey = "Fly"
+    private let shootActionKey = "Shoot"
     
     override func didMove(to view: SKView) {
         
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
-        
         // Create shape node to use during mouse interaction
         let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
+        self.pilotNode = SKSpriteNode(imageNamed: "Fly (1)")
+        self.pilotNode?.size = CGSize(width: w * 1.5, height: w)
         
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
+        self.flyAction = SKAction.repeatForever(SKAction.animate(with: self.flightAnimation,
+                                                                 timePerFrame: 0.15))
+        self.shootAction = SKAction.repeatForever(SKAction.animate(with: self.shootAnimation,
+                                                                   timePerFrame: 0.15))
+        self.pilotNode.run(self.flyAction, withKey: self.flyActionKey)
+        self.pilotNode.position = CGPoint(x: -250, y: -100)
+        self.addChild(self.pilotNode)
     }
     
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
-    }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
-    }
+
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
+        if let touch = touches.first {
+            self.pilotNode.position.y = touch.location(in: self).y
+            self.pilotNode.removeAction(forKey: self.flyActionKey)
+            self.pilotNode.run(self.shootAction, withKey: self.shootActionKey)
         }
-        
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
     }
     
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
+        if let touch = touches.first {
+            self.pilotNode.position.y = touch.location(in: self).y
+        }
     }
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+        if let touch = touches.first {
+            self.pilotNode.position.y = touch.location(in: self).y
+            self.pilotNode.removeAction(forKey: self.shootActionKey)
+            self.pilotNode.run(self.flyAction, withKey: self.flyActionKey)
+        }
     }
     
     override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
+        if let touch = touches.first {
+            self.pilotNode.position.y = touch.location(in: self).y
+            self.pilotNode.position.y = touch.location(in: self).y
+            self.pilotNode.removeAction(forKey: self.shootActionKey)
+            self.pilotNode.run(self.flyAction, withKey: self.flyActionKey)
+        }
+   }
     
     
     override func update(_ currentTime: TimeInterval) {
